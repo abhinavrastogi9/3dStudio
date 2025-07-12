ea41bd5d94a667c114675699b366f8594e01eb7f
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2025-07-12 14:17:54 +0530

    added changes

--- backend/src/Models/userModel.js
+++ backend/src/Models/userModel.js
@@ -32,6 +32,9 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 userSchema.pre("save", async function (next) {
+  if (!this.isModified("hash_password")) {
+    return next();
+  }
   this.hash_password = await bcrypt.hash(this.hash_password, 10);
   next();
 });
